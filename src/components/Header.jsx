70b17bf9fc8f4d29e1ed74c2e0d42180f935abb5
Courtney Schild <courtneyschild@gmail.com>
2020-02-14 13:27:28 -0800

    add and style Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from '../assets/images/logo512.png';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  const headerStyle = {
+    height: '64px',
+    backgroundColor: '#4ECDC4',
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0 16px',
+  }
+  const brandStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  }
+  const navStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  }
+  return (
+    <div style={headerStyle}>
+      <div style={navStyle}>
+        <Link className='left-link' to="/">Home</Link>
+        <Link className='left-link' to="/products">Products</Link>
+      </div>
+      <Link style={brandStyle} to="/">
+        <img src={logo} alt='logo' height='32px'/>
+        <p className='brand-header'>mucha</p>
+      </Link>
+      <div style={navStyle}>
+        <Link className='right-link' to="/signup">Sign Up</Link>
+        <Link className='right-link' to="/signin">Sign In</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
