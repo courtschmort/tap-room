4396efeecc9b42fc5971b83630a11a31c2b36285
Courtney Schild <courtneyschild@gmail.com>
2020-02-14 18:01:05 -0800

    add button and text-shadow.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Main.css';
+import { Link } from 'react-router-dom';
 
 function Main() {
   const mainStyle = {
     height: '100vh',
     display: 'flex',
-    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
     padding: '0 64px',
+  }
+  const buttonStyle = {
+    margin: '64px 0',
   }
   return (
     <div className='background' style={mainStyle}>
       <h1>When life gives us lemons, we make kombucha—lots of it.</h1>
+      <div style={buttonStyle}>
+        <Link className='button' to='/products'>Show me the buch!</Link>
+      </div>
     </div>
   );
 }
