3441275316222df0c021553dbf20d181c5528b48
Courtney Schild <courtneyschild@gmail.com>
2020-02-21 09:27:16 -0800

    update content, add button, and style.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
     padding: '0 64px',
   }
   const h1Style = {
-    margin: '0',
+    margin: '0 0 64px 0',
     paddingBottom: '32px',
     fontFamily: "'Quicksand', sans-serif",
     fontSize: '4em',
@@ -27,16 +27,13 @@ function Home() {
     lineHeight: '1.5',
     color: '#333',
   }
-  const buttonStyle = {
-    margin: '64px 0',
-  }
   return (
     <div className='background-photo'>
       <div style={mainStyle}>
-        <h1 style={h1Style}>When life gives us lemons, we make kombucha—lots of it.</h1>
-        <p style={subStyle}>Migas unicorn la croix gentrify chillwave bitters dreamcatcher raclette retro. Marfa iceland man bun, man braid meggings kombucha wolf salvia post-ironic. Gentrify fam narwhal forage. Wolf cloud bread semiotics neutra. Ennui wolf normcore iceland man braid tbh neutra beard bitters bespoke truffaut marfa wayfarers cloud bread green juice. Four dollar toast meditation direct trade tousled fashion axe small batch air plant sartorial enamel pin godard.</p>
-        <div style={buttonStyle}>
-          <Link className='button' to='/products'>Show me the buch!</Link>
+        <h1 style={h1Style}>When life gives us lemons, we drink kombucha—lots of it.</h1>
+        <div>
+          <Link className='button' to='/products'>Show me the buch</Link>
+          <Link className='button' to='/newproduct'>Add to the buch</Link>
         </div>
       </div>
       <SocialMedia/>
