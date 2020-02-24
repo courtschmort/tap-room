e70236687d76a69df9c575046273de6afef33aaa
Courtney Schild <courtneyschild@gmail.com>
2020-02-23 20:09:34 -0800

    style Error404.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import './Error404.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function Error404(props){
+
+  const styleError404 = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: '100vh',
+    backgroundColor: '#1A535C',
+    padding: '0 64px'
+  }
+
+  const styleHeader = {
+    margin: '0',
+    paddingBottom: '16px',
+    fontFamily: "'Quicksand', sans-serif",
+    fontSize: '3em',
+    fontWeight: '500',
+    color: 'white'
+  }
+
   return (
-    <div>
-      <h2>The page {props.location.pathname} does not exist!</h2>
-      <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+    <div style={styleError404}>
+      <h1 style={styleHeader}>Oops! It seems the page {props.location.pathname} does not exist. You should probably just go <Link className='link' to="/">home</Link>.</h1>
     </div>
   );
 }
